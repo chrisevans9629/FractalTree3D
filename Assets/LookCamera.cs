1216876f5df93972099621eded9bf49307ed2304
Chris Evans <chrisevans9629@gmail.com>
2020-05-10 22:34:35 -0500

    added moving camera

---
+++ Assets/LookCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCamera : MonoBehaviour
+{
+    public float Speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(Speed, Vector3.up);
+    }
+}
