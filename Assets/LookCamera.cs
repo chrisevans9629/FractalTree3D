d9e6daab011e0a4539273e4f4a8fd602da49c355
Chris Evans <chrisevans9629@gmail.com>
2020-05-10 23:37:54 -0500

    added more panning

--- Assets/LookCamera.cs
+++ Assets/LookCamera.cs
@@ -6,8 +6,8 @@ public class LookCamera : MonoBehaviour
 {
     public float Speed = 1;
     public float ZoomSpeed = 1;
-    public float PanSpeed = 1;
-
+    public float PanYSpeed = 1;
+    public float PanXSpeed = 1;
     private Camera ChildCamera;
 
     // Start is called before the first frame update
@@ -28,9 +28,11 @@ public class LookCamera : MonoBehaviour
         //transform.rotation *= Quaternion.AngleAxis(, Vector3.right);
        // transform.rotation *= Quaternion.AngleAxis(Speed, Vector3.up);
 
-        transform.Rotate(0,Speed,0, Space.World);
+       var test = PanXSpeed * (Down(KeyCode.A) - Down(KeyCode.D));
 
-        transform.Rotate( PanSpeed * (Down(KeyCode.W) - Down(KeyCode.S)),0,0, Space.Self);
+        transform.Rotate(0,Speed + test,0, Space.World);
+
+        transform.Rotate( PanYSpeed * (Down(KeyCode.W) - Down(KeyCode.S)),0,0, Space.Self);
 
         ChildCamera.transform.position += ChildCamera.transform.forward * Input.mouseScrollDelta.y * ZoomSpeed;
     }
