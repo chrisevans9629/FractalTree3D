053816e100b1fedd06d60a95af20721314107ca0
Chris Evans <chrisevans9629@gmail.com>
2020-05-12 18:14:52 -0500

    commented out unused code

--- Assets/BranchCreator.cs
+++ Assets/BranchCreator.cs
@@ -3,88 +3,88 @@ using UnityEngine;
 
 
 
-public class BranchCreator : IBranchCreator
-{
-    private readonly TreeGeneratorBase _gen;
-    private readonly IBranchUpdator _branchUpdator;
+//public class BranchCreator : IBranchCreator
+//{
+//    private readonly TreeGeneratorBase _gen;
+//    private readonly IBranchUpdator _branchUpdator;
 
-    public BranchCreator(TreeGeneratorBase gen, IBranchUpdator branchUpdator)
-    {
-        _gen = gen;
-        _branchUpdator = branchUpdator;
-    }
+//    public BranchCreator(TreeGeneratorBase gen, IBranchUpdator branchUpdator)
+//    {
+//        _gen = gen;
+//        _branchUpdator = branchUpdator;
+//    }
 
-    void CreateLeaf(GameObject obj)
-    {
-        if (!_gen.ShouldCreateLeaves)
-            return;
-        var top = _gen.GetTop(obj);
-        GameObject.Instantiate(_gen.LeafPrefab, top.top - top.add / 2f, obj.transform.rotation, _gen.transform);
-    }
-
-
-
-    private GameObject CreateBranch(GameObject obj, float angle)
-    {
-        var obj2 = GameObject.Instantiate(_gen.BranchPrefab, Vector3.zero,
-            Quaternion.identity, _gen.transform);
-        return _branchUpdator.UpdateBranch(obj, angle, obj2);
-    }
-    public List<Branch> CreateSection(GameObject obj, int branchSectionCount, int branchCount = 1)
-    {
-        if (obj.transform.localScale.y < _gen.MinScale)
-        {
-            CreateLeaf(obj);
-            return new List<Branch>();
-        }
-
-        if (branchSectionCount > _gen.MaxBranchSections)
-        {
-            CreateLeaf(obj);
-            return new List<Branch>();
-
-        }
-        if (branchCount > _gen.MaxBranches)
-        {
-            CreateLeaf(obj);
-            return new List<Branch>();
-        }
-
-        var mainAngle = 360f / _gen.BranchingCount;
-
-        GameObject[] branches = new GameObject[_gen.BranchingCount];
-
-        for (int i = 0; i < _gen.BranchingCount; i++)
-        {
-            var a = CreateBranch(obj, i * mainAngle);
+//    void CreateLeaf(GameObject obj)
+//    {
+//        if (!_gen.ShouldCreateLeaves)
+//            return;
+//        var top = _gen.GetTop(obj);
+//        GameObject.Instantiate(_gen.LeafPrefab, top.top - top.add / 2f, obj.transform.rotation, _gen.transform);
+//    }
 
 
-            branches[i] = a;
-        }
+
+//    private GameObject CreateBranch(GameObject obj, float angle)
+//    {
+//        var obj2 = GameObject.Instantiate(_gen.BranchPrefab, Vector3.zero,
+//            Quaternion.identity, _gen.transform);
+//        return _branchUpdator.UpdateBranch(obj, angle, obj2);
+//    }
+//    public List<Branch> CreateSection(GameObject obj, int branchSectionCount, int branchCount = 1)
+//    {
+//        if (obj.transform.localScale.y < _gen.MinScale)
+//        {
+//            CreateLeaf(obj);
+//            return new List<Branch>();
+//        }
+
+//        if (branchSectionCount > _gen.MaxBranchSections)
+//        {
+//            CreateLeaf(obj);
+//            return new List<Branch>();
+
+//        }
+//        if (branchCount > _gen.MaxBranches)
+//        {
+//            CreateLeaf(obj);
+//            return new List<Branch>();
+//        }
+
+//        var mainAngle = 360f / _gen.BranchingCount;
+
+//        GameObject[] branches = new GameObject[_gen.BranchingCount];
+
+//        for (int i = 0; i < _gen.BranchingCount; i++)
+//        {
+//            var a = CreateBranch(obj, i * mainAngle);
 
 
-        var list = new List<Branch>();
-        foreach (var branch in branches)
-        {
-            var section = CreateSection(branch, branchSectionCount + 1, branchCount + _gen.BranchingCount);
-            list.Add(new Branch() { GameObject = branch, Branches = section });
-        }
+//            branches[i] = a;
+//        }
 
-        return list;
-    }
 
-    public void Generate()
-    {
-        var obj = UnityEngine.Object.Instantiate(_gen.BranchPrefab, _gen.transform.position, _gen.transform.rotation, _gen.transform);
-        _gen.Branch = new Branch
-        {
-            GameObject = obj,
-        };
-        _gen.Branch.Branches = CreateSection(_gen.Branch);
-    }
+//        var list = new List<Branch>();
+//        foreach (var branch in branches)
+//        {
+//            var section = CreateSection(branch, branchSectionCount + 1, branchCount + _gen.BranchingCount);
+//            list.Add(new Branch() { GameObject = branch, Branches = section });
+//        }
 
-    public List<Branch> CreateSection(Branch parent)
-    {
-        return CreateSection(parent.GameObject,1,1);
-    }
-}
+//        return list;
+//    }
+
+//    public void Generate()
+//    {
+//        var obj = UnityEngine.Object.Instantiate(_gen.BranchPrefab, _gen.transform.position, _gen.transform.rotation, _gen.transform);
+//        _gen.Branch = new Branch
+//        {
+//            GameObject = obj,
+//        };
+//        _gen.Branch.Branches = CreateSection(_gen.Branch);
+//    }
+
+//    public List<Branch> CreateSection(Branch parent)
+//    {
+//        return CreateSection(parent.GameObject,1,1);
+//    }
+//}
--- Assets/TreeGeneratorInstant.cs
+++ Assets/TreeGeneratorInstant.cs
@@ -1,27 +1,27 @@
-﻿public class TreeGeneratorInstant : ITreeGeneratorType
-{
-    private readonly IBranchCreator _branchCreator;
-    private ITreeUpdator _treeUpdator;
-    public TreeGeneratorInstant(
-        IBranchCreator branchCreator,
-        ITreeUpdator treeUpdator)
-    {
-        _treeUpdator = treeUpdator;
-        _branchCreator = branchCreator;
-    }
+﻿//public class TreeGeneratorInstant : ITreeGeneratorType
+//{
+//    private readonly IBranchCreator _branchCreator;
+//    private ITreeUpdator _treeUpdator;
+//    public TreeGeneratorInstant(
+//        IBranchCreator branchCreator,
+//        ITreeUpdator treeUpdator)
+//    {
+//        _treeUpdator = treeUpdator;
+//        _branchCreator = branchCreator;
+//    }
 
-    public void Generate()
-    {
-        _branchCreator.Generate();
+//    public void Generate()
+//    {
+//        _branchCreator.Generate();
         
-    }
+//    }
 
-    public void Update()
-    {
-        _treeUpdator.Update();
-    }
+//    public void Update()
+//    {
+//        _treeUpdator.Update();
+//    }
 
     
-}
+//}
 
 
