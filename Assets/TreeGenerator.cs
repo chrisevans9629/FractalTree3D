d02ffdf0f5965dc8c314bc3bbe92024a18ecb2cf
Chris Evans <chrisevans9629@gmail.com>
2020-05-11 21:08:51 -0500

    working on adding animation

--- Assets/TreeGenerator.cs
+++ Assets/TreeGenerator.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
 
 
 public class TreeGeneratorAnimated : ITreeGeneratorType
 {
+    private readonly TreeGeneratorBase _gen;
+    public float AnimationSpeed;
+    public TreeGeneratorAnimated(TreeGeneratorBase gen)
+    {
+        _gen = gen;
+    }
+
     public void Generate()
     {
+        GameObject.Instantiate(_gen.BranchPrefab, _gen.transform.position, _gen.transform.rotation, _gen.transform);
     }
 
     public void Update()
@@ -22,7 +32,8 @@ public class TreeGenerator : TreeGeneratorBase
     // Start is called before the first frame update
     void Start()
     {
-        instant = new TreeGeneratorInstant(new BranchCreator(this, new BranchUpdator(this)), new TreeUpdator(this, new BranchUpdator(this)));
+        //instant = new TreeGeneratorInstant(new BranchCreator(this, new BranchUpdator(this)), new TreeUpdator(this, new BranchUpdator(this)));
+        instant = new TreeGeneratorAnimated(this);
 
         instant.Generate();
         PreviousRotateAngle = RotateAngle;
