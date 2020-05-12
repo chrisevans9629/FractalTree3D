f37e074fde71b7b07a867fff748bbe287edaf8ad
Chris Evans <chrisevans9629@gmail.com>
2020-05-12 18:42:26 -0500

    pushed version

--- Assets/TreeGenerator.cs
+++ Assets/TreeGenerator.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.GameCenter;
 
 public class NoBranchCreator : IBranchCreator
 {
